ad08d3d511ac8333727676aac292c97ba82eb1d1
VCGraphicDesign <g.d.chile@gmail.com>
2026-01-19 08:33:40 -0300

    fix: PWA manifest and service worker for valid package generation

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
 const CACHE_NAME = 'audio-cutter-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './index.tsx',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
